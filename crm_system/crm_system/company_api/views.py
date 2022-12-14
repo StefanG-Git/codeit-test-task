7342e95799ba636689b97f7b179b6ec3657b313a
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-14 21:25:03 +0200

    Created `company_create` view

--- crm_system/crm_system/company_api/views.py
+++ crm_system/crm_system/company_api/views.py
@@ -10,3 +10,13 @@ def company_list(request):
     companies = Company.objects.all()
     serializer = CompanySerializer(companies, many=True)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def company_create(request):
+    serializer = CompanySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors)
