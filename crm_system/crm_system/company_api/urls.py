8b386c500ba1e22c5a96edd0250cc43bab570f70
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-14 21:50:48 +0200

    Added url for `company` view

--- crm_system/crm_system/company_api/urls.py
+++ crm_system/crm_system/company_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from crm_system.company_api.views import company_list, company_create
+from crm_system.company_api.views import company_list, company_create, company
 
 urlpatterns = [
     path('list/', company_list, name='company list'),
     path('create/', company_create, name='company create'),
+    path('<int:pk>/', company, name='company action'),
 ]
