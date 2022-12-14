637d0bcd9143d9f02454ce1f59717599f280b6cd
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-14 21:29:01 +0200

    Implemented create method in `CompanySerializer`

--- crm_system/crm_system/company_api/serializer.py
+++ crm_system/crm_system/company_api/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 
+from crm_system.company_api.models import Company
+
 
 class CompanySerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField()
     description = serializers.CharField()
+
+    def create(self, data):
+        return Company.objects.create(**data)
