56bc5dcd231febbed33c3a828d3e69376854f093
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-14 20:56:59 +0200

    Added `rest_framework` to `INSTALLED_APPS`

--- crm_system/crm_system/settings.py
+++ crm_system/crm_system/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'crm_system.company_api'
+    'crm_system.company_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
