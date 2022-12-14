83c12627032a1cc744fba507b3ac46a6fc660abd
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-14 20:40:53 +0200

    Created `companies_list` view

--- crm_system/crm_system/company_api/views.py
+++ crm_system/crm_system/company_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from crm_system.company_api.models import Company
 
-# Create your views here.
+
+def companies_list(request):
+    companies = Company.objects.all()
+    companies_python = list(companies.values())
+    return JsonResponse({
+        'companies': companies_python
+    })
