525ca1525f2199b4311a8abcafda28d3f7d4438d
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-14 21:18:55 +0200

    Edited view name

--- crm_system/crm_system/company_api/urls.py
+++ crm_system/crm_system/company_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from crm_system.company_api.views import companies_list
+from crm_system.company_api.views import company_list
 
 urlpatterns = [
-    path('list/', companies_list, name='companies list'),
+    path('list/', company_list, name='companies list'),
 ]
