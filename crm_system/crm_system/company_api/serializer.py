7581136ea8976ea691ea3cec8b9c2ea729093f57
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-14 21:08:40 +0200

    Created `CompanySerializer`

---
+++ crm_system/crm_system/company_api/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+class CompanySerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    description = serializers.CharField()
