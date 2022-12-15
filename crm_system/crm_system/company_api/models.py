05b4ee4629e2d00fe5a110baa21d324074141f92
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-15 21:37:21 +0200

    Added unique constraint to Company model

--- crm_system/crm_system/company_api/models.py
+++ crm_system/crm_system/company_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     description = models.TextField(max_length=300)
 
     def __str__(self):
