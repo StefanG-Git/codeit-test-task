8d78a87b1334a0e3ff8c4f3beb0c6b74bd47af57
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-15 21:45:47 +0200

    Changed from function-based views to class-based

--- crm_system/crm_system/company_api/urls.py
+++ crm_system/crm_system/company_api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from crm_system.company_api.views import company_list, company_create, company
+from crm_system.company_api.views import CompanyList, CompanyCreate, CompanyView
 
 urlpatterns = [
-    path('list/', company_list, name='company list'),
-    path('create/', company_create, name='company create'),
-    path('<int:pk>/', company, name='company action'),
+    path('list/', CompanyList.as_view(), name='company list'),
+    path('create/', CompanyCreate.as_view(), name='company create'),
+    path('<int:pk>/', CompanyView.as_view(), name='company action'),
 ]
--- crm_system/crm_system/company_api/views.py
+++ crm_system/crm_system/company_api/views.py
@@ -1,47 +1,51 @@
+from django.http.response import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.views import APIView
+
 from crm_system.company_api.models import Company
 from crm_system.company_api.serializer import CompanySerializer
 
 
-@api_view(['GET'])
-def company_list(request):
-    companies = Company.objects.all()
-    serializer = CompanySerializer(companies, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def company_create(request):
-    serializer = CompanySerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
+class CompanyList(APIView):
+    def get(self, request):
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
         return Response(serializer.data)
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def company(request, pk):
-    try:
-        current_company = Company.objects.get(pk=pk)
-    except ObjectDoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = CompanySerializer(current_company)
-        return Response(serializer.data)
-
-    if request.method == 'PUT':
-        serializer = CompanySerializer(current_company, data=request.data)
+class CompanyCreate(APIView):
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'DELETE':
-        current_company.delete()
+
+class CompanyView(APIView):
+    def get_company_by_pk(self, pk):
+        try:
+            return Company.objects.get(pk=pk)
+        except Company.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        company = self.get_company_by_pk(pk)
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        company = self.get_company_by_pk(pk)
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        company = self.get_company_by_pk(pk)
+        company.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
