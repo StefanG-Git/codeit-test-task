ddc0c848942cbde4e1a798c6e9466afc717334e1
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-15 21:45:09 +0200

    Updated `CompanySerializer`

--- crm_system/crm_system/company_api/serializer.py
+++ crm_system/crm_system/company_api/serializer.py
@@ -3,18 +3,7 @@ from rest_framework import serializers
 from crm_system.company_api.models import Company
 
 
-class CompanySerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-
-    def create(self, data):
-        return Company.objects.create(**data)
-
-    def update(self, instance, data):
-        instance.name = data.get('name', instance.name)
-        instance.description = data.get('description', instance.description)
-
-        instance.save()
-
-        return instance
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
