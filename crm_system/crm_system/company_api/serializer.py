a7f3ea6092f9bf4f5d759ff72a511d76114db742
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-16 17:29:04 +0200

    Modified CompanySerializer

--- crm_system/crm_system/company_api/serializer.py
+++ crm_system/crm_system/company_api/serializer.py
@@ -4,6 +4,8 @@ from crm_system.company_api.models import Company
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True)
+
     class Meta:
         model = Company
         fields = '__all__'
