86e1f19def0303ba661637184e59411f36955d87
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-16 17:13:42 +0200

    Added logo field

--- crm_system/crm_system/company_api/models.py
+++ crm_system/crm_system/company_api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=30, unique=True)
+    logo = models.ImageField(upload_to='company_logos/', default='uploads/default.jpg')
     description = models.TextField(max_length=300)
 
     def __str__(self):
