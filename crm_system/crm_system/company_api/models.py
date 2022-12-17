504125955999418496b6e6a778d549a56da1c0c7
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-17 12:01:50 +0200

    Added class attributes

--- crm_system/crm_system/company_api/models.py
+++ crm_system/crm_system/company_api/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    logo = models.ImageField(upload_to='company_logos/')
-    description = models.TextField(max_length=300)
+    NAME_MAX_LENGTH = 30
+    DESCRIPTION_MAX_LENGTH = 300
+    LOGO_UPLOAD_FOLDER = 'company_logos/'
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    logo = models.ImageField(upload_to=LOGO_UPLOAD_FOLDER)
+    description = models.TextField(max_length=DESCRIPTION_MAX_LENGTH)
 
     def __str__(self):
         return self.name
