4e644077c4df3ac6d8d4c32a37c8ede291594353
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-17 12:34:43 +0200

    Created `Employee` model

--- crm_system/crm_system/employee_api/models.py
+++ crm_system/crm_system/employee_api/models.py
@@ -1,3 +1,52 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from crm_system.company_api.models import Company
+
+
+class Employee(models.Model):
+    FIRST_NAME_MIN_LENGTH = 2
+    FIRST_NAME_MAX_LENGTH = 30
+
+    LAST_NAME_MIN_LENGTH = 2
+    LAST_NAME_MAX_LENGTH = 30
+
+    POSITION_MIN_LENGTH = 3
+    POSITION_MAX_LENGTH = 50
+
+    PHOTO_UPLOAD_FOLDER = 'employee_photos/'
+
+    SALARY_DECIMAL_PLACES = 2
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(FIRST_NAME_MIN_LENGTH),
+        )
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(LAST_NAME_MIN_LENGTH),
+        )
+    )
+
+    date_of_birth = models.DateField()
+    photo = models.ImageField(upload_to=PHOTO_UPLOAD_FOLDER)
+    position = models.CharField(
+        max_length=POSITION_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(POSITION_MIN_LENGTH),
+        ),
+    )
+
+    salary = models.DecimalField(
+        max_digits=6,
+        decimal_places=SALARY_DECIMAL_PLACES,
+    )
+
+    company_id = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+    )
