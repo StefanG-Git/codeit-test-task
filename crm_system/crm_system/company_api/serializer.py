ffb3f0cb800e69df6ba3fcf67de3b2b5b8412ddf
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-18 11:36:00 +0200

    Edited `validate_logo` method

--- crm_system/crm_system/company_api/serializer.py
+++ crm_system/crm_system/company_api/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from crm_system.common.utils import image_size_is_valid
 from crm_system.company_api.models import Company
 
 
@@ -11,7 +12,7 @@ class CompanySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_logo(self, image):
-        if image.size > self.LOGO_MAX_SIZE_IN_MB * 1024 * 1024:
+        if not image_size_is_valid(image, self.LOGO_MAX_SIZE_IN_MB):
             raise serializers.ValidationError(f'Max file size is {self.LOGO_MAX_SIZE_IN_MB}MB')
 
         return image
