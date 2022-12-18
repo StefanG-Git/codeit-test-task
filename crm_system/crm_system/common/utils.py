f2d46508c45c2b9d33a4a24fc97e26ffca4001fc
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-18 11:30:30 +0200

    Edited doc string

--- crm_system/crm_system/common/utils.py
+++ crm_system/crm_system/common/utils.py
@@ -23,7 +23,7 @@ def calculate_years_between_dates(start_date: datetime.date, end_date: datetime.
 
 def image_size_is_valid(image: InMemoryUploadedFile, max_size_in_mb: int) -> bool:
     """
-    Validates if the size of the image is not more than the max allowed size.
+    Validates if the size of the image is not more than the allowed size.
 
     :return: bool
     """
