f5f7ce7249e0d10c27862456ce4be2a61d063426
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-20 12:50:05 +0200

    Removed hardcore values and added comments

--- crm_system/crm_system/company_api/tests/test_company_detail_api_view.py
+++ crm_system/crm_system/company_api/tests/test_company_detail_api_view.py
@@ -12,16 +12,18 @@ class TestCompanyDetailAPIView(CompanyAPIViewTestCase):
     UPDATED_LOGO_PATH = 'media/for_testing/updated_valid_size_logo.png'
     UPDATED_DESCRIPTION = 'Updated test description'
 
-    def test_get__when_pk_does_not_exists__expect_not_found(self):
-        expected_info_message = 'Not found.'
+    NOT_FOUND_MESSAGE = 'Not found.'
 
+    def test_get__when_pk_does_not_exists__expect_not_found(self):
         response = self.client.get(reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': 1}))
 
-        self.assertEqual(expected_info_message, response.json()['detail'])
+        self.assertEqual(self.NOT_FOUND_MESSAGE, response.json()['detail'])
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
 
     def test_get__when_pk_exists__expect_to_get_company_with_pk(self):
+        # Create company before get request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+
         response = self.client.get(reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}))
 
         self.assertEqual(self.VALID_NAME, response.data[self.NAME_FIELD])
@@ -30,20 +32,19 @@ class TestCompanyDetailAPIView(CompanyAPIViewTestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_put__when_pk_does_not_exists__expect_not_found(self):
-        expected_info_message = 'Not found.'
-
         response = self.client.put(reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': 1}))
 
-        self.assertEqual(expected_info_message, response.json()['detail'])
+        self.assertEqual(self.NOT_FOUND_MESSAGE, response.json()['detail'])
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
 
     def test_put__when_pk_exists_and_update_all_fields_with_valid_data__expect_to_update_all_company_fields(self):
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
-
-        updated_image = create_image(self.TEST_LOGO_NAME, self.UPDATED_LOGO_PATH)
+        # Change every value of the fields with new one
+        updated_logo = create_image(self.TEST_LOGO_NAME, self.UPDATED_LOGO_PATH)
         updated_test_data = {
             self.NAME_FIELD: self.UPDATED_NAME,
-            self.LOGO_FIELD: updated_image,
+            self.LOGO_FIELD: updated_logo,
             self.DESCRIPTION_FIELD: self.UPDATED_DESCRIPTION,
         }
         # Delete the initial logo because it's creating new with random name
@@ -60,7 +61,9 @@ class TestCompanyDetailAPIView(CompanyAPIViewTestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_name_with_valid_data__expect_to_update_name_only(self):
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+        # Change name value with new one
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
             data={self.NAME_FIELD: self.UPDATED_NAME}
@@ -72,39 +75,40 @@ class TestCompanyDetailAPIView(CompanyAPIViewTestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_name_with_empty_str__expect_bad_request(self):
-        expected_info_messages = ['This field may not be blank.']
-
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+        # Change name value to empty string
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
             data={self.NAME_FIELD: ''}
         )
 
-        self.assertListEqual(expected_info_messages, response.json()[self.NAME_FIELD])
+        self.assertListEqual(self.BLANK_FIELD_MESSAGE, response.json()[self.NAME_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_name_with_too_long_value__expect_bad_request(self):
-        expected_info_messages = ['Ensure this field has no more than 30 characters.']
-
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+        # Change name value to too long string
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
             data={self.NAME_FIELD: self.TOO_LONG_NAME}
         )
 
-        self.assertListEqual(expected_info_messages, response.json()[self.NAME_FIELD])
+        self.assertListEqual(self.MAX_30_CHARS_MESSAGE, response.json()[self.NAME_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_logo_with_valid_value__expect_to_update_logo_only(self):
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
-
-        updated_image = create_image(self.TEST_LOGO_NAME, self.UPDATED_LOGO_PATH)
+        # Create new logo
+        updated_logo = create_image(self.TEST_LOGO_NAME, self.UPDATED_LOGO_PATH)
         # Delete the initial logo because it's creating new with random name
         delete_file(self.TEST_LOGO_TO_DELETE_PATH)
-
+        # Change logo value with the new one
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
-            data={self.LOGO_FIELD: updated_image}
+            data={self.LOGO_FIELD: updated_logo}
         )
 
         self.assertEqual(self.VALID_NAME, response.data[self.NAME_FIELD])
@@ -113,33 +117,35 @@ class TestCompanyDetailAPIView(CompanyAPIViewTestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_logo_with_too_big_file__expect_bad_request(self):
-        expected_info_messages = ['Max file size is 1MB']
-
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
-        updated_image = create_image(self.TEST_LOGO_NAME, self.TOO_BIG_LOGO_PATH)
-
+        # Create new logo with size greater than acceptable
+        updated_logo = create_image(self.TEST_LOGO_NAME, self.TOO_BIG_LOGO_PATH)
+        # Change logo value with the new one
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
-            data={self.LOGO_FIELD: updated_image}
+            data={self.LOGO_FIELD: updated_logo}
         )
 
-        self.assertListEqual(expected_info_messages, response.json()[self.LOGO_FIELD])
+        self.assertListEqual(self.MAX_1_MB_MESSAGE, response.json()[self.LOGO_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_logo_with_not_a_file_value__expect_bad_request(self):
-        expected_info_messages = ['The submitted data was not a file. Check the encoding type on the form.']
-
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+        # Change logo value with string
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
             data={self.LOGO_FIELD: self.NAME_FIELD}
         )
 
-        self.assertListEqual(expected_info_messages, response.json()[self.LOGO_FIELD])
+        self.assertListEqual(self.INVALID_FILE_FORMAT_MESSAGE, response.json()[self.LOGO_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_description_with_valid_value__expect_to_update_description_only(self):
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+        # Change name value with new one
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
             data={self.DESCRIPTION_FIELD: self.UPDATED_DESCRIPTION}
@@ -151,42 +157,40 @@ class TestCompanyDetailAPIView(CompanyAPIViewTestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_description_with_empty_str__expect_bad_request(self):
-        expected_info_messages = ['This field may not be blank.']
-
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+        # Change description value to empty string
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
             data={self.DESCRIPTION_FIELD: ''}
         )
 
-        self.assertListEqual(expected_info_messages, response.json()[self.DESCRIPTION_FIELD])
+        self.assertListEqual(self.BLANK_FIELD_MESSAGE, response.json()[self.DESCRIPTION_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_put__when_pk_exists_and_update_only_description_with_too_long_value__expect_bad_request(self):
-        expected_info_messages = ['Ensure this field has no more than 300 characters.']
-
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+        # Change description value to too long string
         response = self.client.put(
             reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}),
             data={self.DESCRIPTION_FIELD: self.TOO_LONG_DESCRIPTION}
         )
 
-        self.assertListEqual(expected_info_messages, response.json()[self.DESCRIPTION_FIELD])
+        self.assertListEqual(self.MAX_300_CHARS_MESSAGE, response.json()[self.DESCRIPTION_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_delete__when_pk_does_not_exists__expect_not_found(self):
-        expected_info_message = 'Not found.'
-
         response = self.client.delete(reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': 1}))
 
-        self.assertEqual(expected_info_message, response.json()['detail'])
+        self.assertEqual(self.NOT_FOUND_MESSAGE, response.json()['detail'])
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
 
     def test_delete__when_pk_exists__expect_to_delete_company_with_pk(self):
-        expected_companies_count = 0
-
+        # Create company before put request
         res = self.client.post(self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+
         response = self.client.delete(reverse(self.COMPANY_DETAIL_URL_NAME, kwargs={'pk': res.json()['id']}))
 
         self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
-        self.assertEqual(self.COMPANY_MODEL.objects.all().count(), expected_companies_count)
+        self.assertEqual(0, self.COMPANY_MODEL.objects.all().count())
