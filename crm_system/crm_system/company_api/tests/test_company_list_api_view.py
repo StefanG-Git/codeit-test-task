955b5199fd694e4ef0842d553a85028c2d8bb5e7
Stefan Geranliev <s.geranliev@abv.bg>
2022-12-20 12:50:15 +0200

    Removed hardcore values and added comments

--- crm_system/crm_system/company_api/tests/test_company_list_api_view.py
+++ crm_system/crm_system/company_api/tests/test_company_list_api_view.py
@@ -5,124 +5,126 @@ from crm_system.company_api.tests.common_company_test_config import CompanyAPIVi
 
 
 class TestCompanyListAPIView(CompanyAPIViewTestCase):
-    def test_get__when_there_are_no_companies__expect_empty_list(self):
-        expected_companies = []
+    REQUIRED_FIELD_MESSAGE = ['This field is required.']
+    NO_FILE_MESSAGE = ['No file was submitted.']
+    COMPANY_EXISTS_MESSAGE = ['company with this name already exists.']
 
+    def test_get__when_there_are_no_companies__expect_empty_list(self):
         response = self.client.get(self.COMPANY_LIST_URL_NAME)
 
-        self.assertListEqual(expected_companies, response.data)
+        self.assertListEqual([], response.data)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_get__when_there_are_companies__expect_to_get_all(self):
-        expected_companies_count = 1
-
+        # Create company before get request
         self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+
         response = self.client.get(self.COMPANY_LIST_URL_NAME)
 
         self.assertEqual(self.VALID_NAME, response.json()[0][self.NAME_FIELD])
         self.assertEqual('/' + self.TEST_LOGO_TO_DELETE_PATH, response.json()[0][self.LOGO_FIELD])
         self.assertEqual(self.VALID_DESCRIPTION, response.json()[0][self.DESCRIPTION_FIELD])
-        self.assertEqual(expected_companies_count, len(response.json()))
+        self.assertEqual(1, len(response.json()))
+        self.assertEqual(1, self.COMPANY_MODEL.objects.all().count())
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_post__when_all_data_is_valid__expect_to_create_new_company(self):
-        expected_companies_count = 1
-
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
         self.assertEqual(self.VALID_NAME, response.json()[self.NAME_FIELD])
         self.assertEqual('/' + self.TEST_LOGO_TO_DELETE_PATH, response.json()[self.LOGO_FIELD])
         self.assertEqual(self.VALID_DESCRIPTION, response.json()[self.DESCRIPTION_FIELD])
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
-        self.assertEqual(self.COMPANY_MODEL.objects.all().count(), expected_companies_count)
+        self.assertEqual(1, self.COMPANY_MODEL.objects.all().count())
 
     def test_post__when_missing_name_value__expect_bad_request(self):
-        expected_info_messages = ['This field is required.']
-
+        # Remove name value before post request
         self.valid_test_data.pop(self.NAME_FIELD)
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.NAME_FIELD])
+        self.assertListEqual(self.REQUIRED_FIELD_MESSAGE, response.json()[self.NAME_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_missing_logo_value__expect_bad_request(self):
-        expected_info_messages = ['No file was submitted.']
-
+        # Remove logo value before post request
         self.valid_test_data.pop(self.LOGO_FIELD)
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.LOGO_FIELD])
+        self.assertListEqual(self.NO_FILE_MESSAGE, response.json()[self.LOGO_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_missing_description_value__expect_bad_request(self):
-        expected_info_messages = ['This field is required.']
-
+        # Remove description value before post request
         self.valid_test_data.pop(self.DESCRIPTION_FIELD)
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.DESCRIPTION_FIELD])
+        self.assertListEqual(self.REQUIRED_FIELD_MESSAGE, response.json()[self.DESCRIPTION_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_name_is_empty_str__expect_bad_request(self):
-        expected_info_messages = ['This field may not be blank.']
-
+        # Change name value to empty string before post request
         self.valid_test_data[self.NAME_FIELD] = ''
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.NAME_FIELD])
+        self.assertListEqual(self.BLANK_FIELD_MESSAGE, response.json()[self.NAME_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_duplicate_name__expect_bad_request(self):
-        expected_info_messages = ['company with this name already exists.']
-
+        # Create company before post request
         self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
+        # Create company with the same name
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.NAME_FIELD])
+        self.assertListEqual(self.COMPANY_EXISTS_MESSAGE, response.json()[self.NAME_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_name_is_too_long__expect_bad_request(self):
-        expected_info_messages = ['Ensure this field has no more than 30 characters.']
-
+        # Change name value to too long string before post request
         self.valid_test_data[self.NAME_FIELD] = self.TOO_LONG_NAME
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.NAME_FIELD])
+        self.assertListEqual(self.MAX_30_CHARS_MESSAGE, response.json()[self.NAME_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_logo_value_is_not_a_file__expect_bad_request(self):
-        expected_info_messages = ['The submitted data was not a file. Check the encoding type on the form.']
-
+        # Change logo value to not a path string before post request
         self.valid_test_data[self.LOGO_FIELD] = self.TOO_LONG_NAME
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.LOGO_FIELD])
+        self.assertListEqual(self.INVALID_FILE_FORMAT_MESSAGE, response.json()[self.LOGO_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_logo_file_is_too_big__expect_bad_request(self):
-        expected_info_messages = ['Max file size is 1MB']
-
+        # Creating logo with size greater than acceptable
         big_image = create_image(self.TEST_LOGO_NAME, self.TOO_BIG_LOGO_PATH)
+        # Change logo value to too big size before post request
         self.valid_test_data[self.LOGO_FIELD] = big_image
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.LOGO_FIELD])
+        self.assertListEqual(self.MAX_1_MB_MESSAGE, response.json()[self.LOGO_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_description_is_empty_str__expect_bad_request(self):
-        expected_info_messages = ['This field may not be blank.']
-
+        # Change description value to empty string before post request
         self.valid_test_data[self.DESCRIPTION_FIELD] = ''
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.DESCRIPTION_FIELD])
+        self.assertListEqual(self.BLANK_FIELD_MESSAGE, response.json()[self.DESCRIPTION_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_post__when_description_is_too_long__expect_bad_request(self):
-        expected_info_messages = ['Ensure this field has no more than 300 characters.']
-
+        # Change description value to too long string before post request
         self.valid_test_data[self.DESCRIPTION_FIELD] = self.TOO_LONG_DESCRIPTION
+
         response = self.client.post(path=self.COMPANY_LIST_URL_NAME, data=self.valid_test_data)
 
-        self.assertListEqual(expected_info_messages, response.json()[self.DESCRIPTION_FIELD])
+        self.assertListEqual(self.MAX_300_CHARS_MESSAGE, response.json()[self.DESCRIPTION_FIELD])
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
